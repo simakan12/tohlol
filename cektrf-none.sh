b2f088b063d2a80dda42ecd0c03fdac6dde348c8
simakan12 <155060518+simakan12@users.noreply.github.com>
2024-01-07 08:59:40 +0700

    Update cektrf-none.sh

--- cektrf-none.sh
+++ cektrf-none.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
 _APISERVER=127.0.0.1:47465
-_Xray=/usr/bin/xray/xray
-
+_XRAY=/usr/bin/xray/xray
 apidata () {
     local ARGS=
     if [[ $1 == "reset" ]]; then
-      ARGS="reset: true"
+      ARGS="-reset=true"
     fi
-    $_Xray api statsquery --server=$_APISERVER "${ARGS}" \
+    $_XRAY api statsquery --server=$_APISERVER "${ARGS}" \
     | awk '{
         if (match($1, /"name":/)) {
             f=1; gsub(/^"|link"|,$/, "", $2);
             split($2, p,  ">>>");
             printf "%s:%s->%s\t", p[1],p[2],p[4];
         }
-        else if (match($1, /"value":/) && f){ f = 0; printf "%.0f\n", $2; }
+        else if (match($1, /"value":/) && f){
+          f = 0;
+          gsub(/"/, "", $2);
+          printf "%.0f\n", $2;
+        }
         else if (match($0, /}/) && f) { f = 0; print 0; }
     }'
 }
-
 print_sum() {
     local DATA="$1"
     local PREFIX="$2"
@@ -34,9 +36,8 @@ print_sum() {
     | numfmt --field=2 --suffix=B --to=iec \
     | column -t
 }
-
 DATA=$(apidata $1)
-echo ""
-echo "==--TRAFFIC USER XRAY NONE TLS--=="
+echo "-----------------------------"
+echo "==--TRAFFIK XRAY-NONTLS--=="
 print_sum "$DATA" "user"
 echo "-----------------------------"
